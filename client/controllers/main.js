c7da231008905b236d8d6cd8542432447984afee
Mac Evans <maclean.evans@gmail.com>
2016-04-24 09:07:07 -0500

    push

--- client/controllers/main.js
+++ client/controllers/main.js
@@ -2,20 +2,41 @@
 
 
 angular.module('myApp')
-  .controller('MainCtrl', ['$scope','Auth','Service', function($scope, Auth, Service) {
+  .controller('MainCtrl', ['$scope','$interval','Auth','Service', function($scope, $interval, Auth, Service) {
   	$scope.welcome = 'MakerMessage';
     $scope.answers = window.answers;
-    $scope.zingArray  = [{username: "Mac", message: "Hey everyone"}, {username: "Mac", message: "Bye everyone"}]//HTTP request to the server 
+    var createdObjs = [answers.zing1, answers.zing2, answers.zing3, answers.zing4];
+    var usernames = [answers.zing1.username, answers.zing2.username, answers.zing3.username, answers.zing4.username];
+
+    // $scope.zingArray  = [{username: "Mac", message: "Hey everyone"}, {username: "Mac", message: "Bye everyone"}]//HTTP request to the server
+    $scope.getZings = function(){
+      Service.getAllZings()
+      .then(function(response){
+        if($scope.answers.sectionFour){
+          $scope.zingArray = response.filter(function(zing){
+            return usernames.indexOf(zing.username) > -1
+          })
+        } else {
+        $scope.zingArray = response
+        }
+      })
+    }
+    $scope.getZings();
+    $interval(function(){
+      $scope.getZings();
+    }, 2000)
+    $scope.zingArray;
     $scope.zing = {
     	username: '',
     	message: ''
     }
-    console.log("answers in the controler", answers);
+
     $scope.errorMessage = '';
   	$scope.user = {
   		username: '',
   		password: ''
   	};
+
   	$scope.login = function() {
   		Auth.login($scope.user)
   		.then(function(resp){
@@ -46,13 +67,19 @@ angular.module('myApp')
   			console.error('error when login attempted: ', error);
   		});
   	};
-  	$scope.zingify = function(){
-  		Service.zingify()
+  	$scope.zingify = function(username, message){
+      console.log($scope.zingArray)
+  		Service.zingify(username, message)
   		.then(function(resp){
-  			console.log('zingify', resp);
+        $scope.username = "";
+        $scope.message = "";
   		})
   		.catch(function(resp){
   			console.log('error sending zing', error);
   		})
   	}
+
+
+    console.log('answer scope', $scope.answers)
+
   }]);
